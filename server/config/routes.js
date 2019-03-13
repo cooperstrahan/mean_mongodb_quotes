29dd420aad57afb230b2bb6eaea6e7a73a9aad00
cstra <cooperstrahan@gmail.com>
2019-03-13 13:52:18 -0700

    modularized with a controller

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,38 +1,7 @@
-const mongoose = require('mongoose'),
-    Quote = mongoose.model('Quote');
+const quotes = require('../controllers/quotes.js');
 
 module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('index');
-    })
-    
-    app.post('/write', function(req, res) {
-        console.log('POST DATA', req.body);
-        var quote = new Quote({name: req.body.name, quote: req.body.quote});
-        quote.save(function(err) {
-            if(err) {
-                console.log('Something went wrong adding a new quote');
-                for(var key in err.errors){
-                    req.flash('entry', err.errors[key].message);
-                }
-                res.redirect('/');
-            } else {
-                console.log('Successfully added quote!');
-                res.redirect('/quotes');
-            }
-        })
-        
-    })
-    
-    app.get('/quotes', function(req, res) {
-        var table = Quote.find({}, function(err, quotes) {
-            if(err) {
-                console.log('Something went wrong getting the quotes');
-                res.render('/');
-            } else {
-                console.log('Successfully retrieved the quotes');
-                res.render('quote', {quote_list: quotes});
-            }
-        });
-    });
+    app.get('/', function(req, res) {quotes.index(req, res);});
+    app.post('/write', function(req, res) {quotes.write(req, res);});
+    app.get('/quotes', function(req, res) {quotes.getquote(req, res);});
 }
---
+++ server/controllers/quotes.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose'),
+    Quote = mongoose.model('Quote');
+    
+module.exports = {
+    index: function(req, res) {
+        res.render('index');
+    },
+    write: function(req, res) {
+        var quote = new Quote({name: req.body.name, quote: req.body.quote});
+        quote.save(function(err) {
+            if(err) {
+                console.log('Something went wrong adding a new quote');
+                for(var key in err.errors){
+                    req.flash('entry', err.errors[key].message);
+                }
+                res.redirect('/');
+            } else {
+                console.log('Successfully added quote!');
+                res.redirect('/quotes');
+            }
+        });
+    },
+    getquote: function(req, res) {
+        var table = Quote.find({}, function(err, quotes) {
+            if(err) {
+                console.log('Something went wrong getting the quotes');
+                res.render('/');
+            } else {
+                console.log('Successfully retrieved the quotes');
+                res.render('quote', {quote_list: quotes});
+            }
+        });
+    }
+};
