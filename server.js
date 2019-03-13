050e64e257fd610a2f19721f44c738cb7291cf55
cstra <cooperstrahan@gmail.com>
2019-03-13 12:44:26 -0700

    routes modularized

--- server.js
+++ server.js
@@ -30,40 +30,6 @@ mongoose.model('Quote', QuoteSchema);
 var Quote = mongoose.model('Quote');
 mongoose.Promise = global.Promise;
 
-app.get('/', function(req, res) {
-    res.render('index');
-})
+app.listen(port, function() {});
 
-app.post('/write', function(req, res) {
-    console.log('POST DATA', req.body);
-    var quote = new Quote({name: req.body.name, quote: req.body.quote});
-    quote.save(function(err) {
-        if(err) {
-            console.log('Something went wrong adding a new quote');
-            for(var key in err.errors){
-                req.flash('entry', err.errors[key].message);
-            }
-            res.redirect('/');
-        } else {
-            console.log('Successfully added quote!');
-            res.redirect('/quotes');
-        }
-    })
-    
-})
-
-app.get('/quotes', function(req, res) {
-    var table = Quote.find({}, function(err, quotes) {
-        if(err) {
-            console.log('Something went wrong getting the quotes');
-            res.render('/');
-        } else {
-            console.log('Successfully retrieved the quotes');
-            res.render('quote', {quote_list: quotes});
-        }
-    })
-})
-
-app.listen(port, function() {
-
-})
+require('./server/config/routes.js')(app);
